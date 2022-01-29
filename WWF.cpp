a97087bdfb87ff72fc6f1727fc7cb1b9cd8d0311
ReioSerg <94328023+ReioSerg@users.noreply.github.com>
2022-01-29 19:15:01 +0300

    Add files via upload

---
+++ WWF.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#include <signal.h>
+using namespace std;
+bool f = true;
+void Ctr_c(int sub) {
+	cout << "Im done\n";
+	f = false;
+}
+
+void main(int argc, char* argv[])
+{   
+	char str[100];
+	ofstream file;
+	file.open(argv[1]);
+	if (file.is_open() == false) {
+		cout << "File cant be open";
+	}
+	else {
+		cout << "Enter the data\n";
+		while (f) {
+			cin.getline(str, 99);
+			file << str<<endl;
+			signal(SIGINT, Ctr_c);
+		}
+	}
+	file.close();
+}
